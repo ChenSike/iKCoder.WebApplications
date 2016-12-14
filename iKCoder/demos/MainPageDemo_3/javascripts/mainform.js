18f1e87f2a42412b650b4b21f9ebf3e0692e1bfb
cliu3 <carl.liu@morningstar.com>
2016-12-14 19:27:31 +0800

    add img

--- iKCoder/demos/MainPageDemo_3/javascripts/mainform.js
+++ iKCoder/demos/MainPageDemo_3/javascripts/mainform.js
@@ -27,7 +27,7 @@ function onLoad() {
             name: '吃豆子 Pac Man',
             rate: 90,
             note: '入门物理, 入门数学',
-            img: 'images/span/Theme_Ball.fw.png',
+            img: 'images/span/Theme_Beans.fw.png',
             diff: '2',
             diffName: 'G2',
             point: 'NPC设定, 路径设定'
@@ -36,7 +36,7 @@ function onLoad() {
             name: '字母大作战 War Of The Chars',
             rate: 80,
             note: '入门英语, 打字',
-            img: 'images/span/Theme_Ball.fw.png',
+            img: 'images/span/Theme_Chars.fw.png',
             diff: '3',
             diffName: 'G3',
             point: '字母设定, 单词设定, 分数设定'
@@ -45,7 +45,7 @@ function onLoad() {
             name: '石头剪刀布',
             rate: 50,
             note: '入门逻辑判断',
-            img: 'images/span/Theme_Ball.fw.png',
+            img: 'images/span/Theme_Wins.fw.png',
             diff: '1',
             diffName: 'G1',
             point: '条件判定'
@@ -54,7 +54,7 @@ function onLoad() {
             name: '弹球控制2 Ball Control2',
             rate: 20,
             note: '入门物理, 入门数学',
-            img: 'images/span/Theme_Ball.fw.png',
+            img: 'images/span/Theme_Beans.fw.png',
             diff: '3',
             diffName: 'G3',
             point: '边界设定, 速度设定'
@@ -64,7 +64,7 @@ function onLoad() {
             name: '吃豆子2 Pac Man2',
             rate: 0,
             note: '入门物理, 入门数学',
-            img: 'images/span/Theme_Ball.fw.png',
+            img: 'images/span/Theme_Wins.fw.png',
             diff: '1',
             diffName: 'G1',
             point: '边界设定, 速度设定'
