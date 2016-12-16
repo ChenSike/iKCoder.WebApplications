901ebba1ef1018cd4ff0267ed8c9571faa4ab44d
cliu3 <carl.liu@morningstar.com>
2016-12-16 17:01:06 +0800

    my lessons page complete

--- iKCoder/demos/MainPageDemo_3/javascripts/mylessons.js
+++ iKCoder/demos/MainPageDemo_3/javascripts/mylessons.js
@@ -12,65 +12,60 @@ function onLoad() {
         "images/TmpAdv/Adv2.fw.png"
     ];
 
-    var liberaryArray = [
+    var lessonsArray = [
         {
             id: '1',
-            name: '弹球控制 Ball Control',
+            cname: '弹球控制',
+            ename: 'Ball Control',
             rate: 100,
-            note: '入门物理, 入门数学',
             img: 'images/span/Theme_Ball.fw.png',
             diff: '1',
-            diffName: 'G1',
-            point: '边界设定, 速度设定'
+            scenecount:5
         }, {
             id: '2',
-            name: '吃豆子 Pac Man',
+            cname: '吃豆子',
+            ename: 'Pac Man',
             rate: 90,
-            note: '入门物理, 入门数学',
             img: 'images/span/Theme_Beans.fw.png',
             diff: '2',
-            diffName: 'G2',
-            point: 'NPC设定, 路径设定'
+            scenecount: 5
         }, {
             id: '3',
-            name: '字母大作战 War Of The Chars',
+            cname: '字母大作战',
+            ename: 'War Of The Chars',
             rate: 80,
-            note: '入门英语, 打字',
             img: 'images/span/Theme_Chars.fw.png',
             diff: '3',
-            diffName: 'G3',
-            point: '字母设定, 单词设定, 分数设定'
+            scenecount: 5
         }, {
             id: '4',
-            name: '石头剪刀布',
+            cname: '石头剪刀布',
+            ename: '',
             rate: 50,
-            note: '入门逻辑判断',
             img: 'images/span/Theme_Wins.fw.png',
             diff: '1',
-            diffName: 'G1',
-            point: '条件判定'
+            scenecount: 5
         }, {
             id: '5',
-            name: '弹球控制2 Ball Control2',
+            cname: '弹球控制2',
+            ename: 'Ball Control2',
             rate: 20,
-            note: '入门物理, 入门数学',
-            img: 'images/span/Theme_Beans.fw.png',
+            img: 'images/span/Theme_Ball.fw.png',
             diff: '3',
-            diffName: 'G3',
-            point: '边界设定, 速度设定'
+            scenecount: 5
         }
         , {
             id: '6',
-            name: '吃豆子2 Pac Man2',
+            cname: '吃豆子 2',
+            ename: 'Pac Man 2',
             rate: 0,
-            note: '入门物理, 入门数学',
-            img: 'images/span/Theme_Wins.fw.png',
+            img: 'images/span/Theme_Beans.fw.png',
             diff: '1',
-            diffName: 'G1',
-            point: '边界设定, 速度设定'
+            scenecount: 5
         }
     ];
 
     buildHeader(false, advertsArray);
+    buildLessons(lessonsArray, lessonsArray);
     buildFooter();
 }
