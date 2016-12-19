faa0ffe123e1b35af1e2c61b91b9b48ded340439
CPLiu2012 <cpliu2001@sina.com>
2016-12-19 21:59:58 +0800

    update

--- iKCoder/javascripts/common/common.js
+++ iKCoder/javascripts/common/common.js
@@ -123,3 +123,22 @@ function DeleteKeyframesRule(keyframesName) {
     return tmpobj.sheet;
 }
 
+function ConvertImgToBase64(url, callback, outputFormat) {
+    var canvas = document.createElement('CANVAS'),
+        ctx = canvas.getContext('2d'),
+        img = new Image;
+    img.crossOrigin = 'Anonymous';
+    img.onload = function () {
+        canvas.height = img.height;
+        canvas.width = img.width;
+        ctx.drawImage(img, 0, 0);
+        var dataURL = canvas.toDataURL(outputFormat || 'image/png');
+        callback.call(this, dataURL);
+        canvas = null;
+    };
+    img.src = url;
+}
+
+//ConvertImgToBase64('http://bit.ly/18g0VNp', function (base64Img) {
+//    // Base64DataURL
+//});
