c7a39b9b2cd470253b784c8ba7e84947449f11b7
cliu3 <carl.liu@morningstar.com>
2017-02-24 19:10:11 +0800

    .

---
+++ iKCoder/javascripts/common/ExtendBlockly.js
@@ -0,0 +1,64 @@
+﻿'use strict';
+
+Blockly.Toolbox.prototype.addColour_ = function (opt_tree) {
+    var tree = opt_tree || this.tree_;
+    var children = tree.getChildren();
+    for (var i = 0, child; child = children[i]; i++) {
+        //var element = child.getRowElement();
+        $(child.getRowElement()).css('border', 'none');
+        var element = child.getElement();
+        if (element) {
+            if (this.hasColours_) {
+                var border = '10px solid ' + (child.hexColour || '#ddd');
+            } else {
+                var border = 'none';
+            }
+            if (this.workspace_.RTL) {
+                element.style.borderRight = border;
+            } else {
+                element.style.borderLeft = border;
+            }
+        }
+        this.addColour_(child);
+    }
+};
+
+Blockly.Toolbox.TreeControl.prototype.setSelectedItem = function (node) {
+    var toolbox = this.toolbox_;
+    if (node == this.selectedItem_ || node == toolbox.tree_) {
+        return;
+    }
+    if (toolbox.lastCategory_) {
+        toolbox.lastCategory_.getElement().style.backgroundColor = "";
+        //toolbox.lastCategory_.getRowElement().style.backgroundColor = '';
+    }
+    if (node) {
+        var hexColour = node.hexColour || '#57e';
+        //node.getRowElement().style.backgroundColor = hexColour;
+        node.getElement().style.backgroundColor = hexColour;
+        // Add colours to child nodes which may have been collapsed and thus
+        // not rendered.
+        toolbox.addColour_(node);
+    }
+    var oldNode = this.getSelectedItem();
+    goog.ui.tree.TreeControl.prototype.setSelectedItem.call(this, node);
+    if (node && node.blocks && node.blocks.length) {
+        toolbox.flyout_.show(node.blocks);
+        // Scroll the flyout to the top if the category has changed.
+        if (toolbox.lastCategory_ != node) {
+            toolbox.flyout_.scrollToStart();
+        }
+    } else {
+        // Hide the flyout.
+        toolbox.flyout_.hide();
+    }
+    if (oldNode != node && oldNode != this) {
+        var event = new Blockly.Events.Ui(null, 'category',
+            oldNode && oldNode.getHtml(), node && node.getHtml());
+        event.workspaceId = toolbox.workspace_.id;
+        Blockly.Events.fire(event);
+    }
+    if (node) {
+        toolbox.lastCategory_ = node;
+    }
+};
