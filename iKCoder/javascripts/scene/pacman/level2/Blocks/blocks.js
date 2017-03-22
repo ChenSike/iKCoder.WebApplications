aa2a3c79e740559dda4ee69d3720f79561c3c9ab
CPLiu2012 <cpliu2001@sina.com>
2017-03-22 22:58:07 +0800

    .

--- iKCoder/javascripts/scene/pacman/level2/Blocks/blocks.js
+++ iKCoder/javascripts/scene/pacman/level2/Blocks/blocks.js
@@ -231,3 +231,77 @@ Blockly.JavaScript["pacman_coord_random"] = function (block) {
         Blockly.JavaScript.ORDER_FUNCTION_CALL
     ];
 };
+
+Blockly.Blocks['event_start'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("当开始运行");
+        this.setNextStatement(true, null);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.JavaScript['event_start'] = function (block) {
+    return 'Scene.start();\n';
+};
+
+Blockly.Blocks['move_onestep_up'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("向上移动一步");
+        this.setNextStatement(true, null);
+        this.setPreviousStatement(true, null);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.JavaScript['move_onestep_up'] = function (block) {
+    return 'Scene.move("U", 1);\n';
+};
+
+Blockly.Blocks['move_onestep_down'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("向下移动一步");
+        this.setNextStatement(true, null);
+        this.setPreviousStatement(true, null);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.JavaScript['move_onestep_down'] = function (block) {
+    return 'Scene.move("D", 1);\n';
+};
+
+Blockly.Blocks['move_onestep_left'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("向左移动一步");
+        this.setNextStatement(true, null);
+        this.setPreviousStatement(true, null);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.JavaScript['move_onestep_left'] = function (block) {
+    return 'Scene.move("L", 1);\n';
+};
+
+Blockly.Blocks['move_onestep_right'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("向右移动一步");
+        this.setNextStatement(true, null);
+        this.setPreviousStatement(true, null);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.JavaScript['move_onestep_right'] = function (block) {
+    return 'Scene.move("R", 1);\n';
+};
